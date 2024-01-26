476710d8fd5c11fafc12719fdb0b8a4f80545c0c
BuseVarkan <buse.varkan@ceng.metu.edu.tr>
2024-01-26 13:27:11 +0300

    Add test cases and simulate demo

--- demo.py
+++ demo.py
@@ -1,1 +1,46 @@
+import requests
 
+api_endpoint = "http://localhost:8000/top2"
+
+test_phrases = []
+
+# Test case 1: Empty list
+phrases = []
+test_phrases.append(phrases)
+
+# Test case 2: List with 1 element
+phrases = ["Buse"]
+test_phrases.append(phrases)
+
+# Test case 3: List with 2 elements
+phrases = ["Buse", "Varkan"]
+test_phrases.append(phrases)
+
+# Test case 4: Phrase is empty
+phrases = ["computer", ""]
+test_phrases.append(phrases)
+
+# Test case 5: Phrase is not a string
+phrases = ["computer", 1]
+test_phrases.append(phrases)
+
+# Test case 6: Phrase with more than 1000 characters
+phrases = ["a" * 1001, "b"]
+test_phrases.append(phrases)
+
+# Test case 7: Valid list with more than 2 elements
+phrases = ["Buse", "Varkan", "apple", "orange", "I love playing video games","Deep learning is a subset of machine learning","Machine learning is a subset of artificial intelligence"]
+test_phrases.append(phrases)
+
+# Get test case number from user
+test_case_number = int(input("Enter test case number (1-7): "))
+
+# Send request to API
+response = requests.post(api_endpoint, json=test_phrases[test_case_number - 1])
+
+if response.status_code == 200:
+    print("Top 2 closest phrases:", response.json())
+
+else:
+    print("Status Code:",response.status_code)
+    print(response.json().get("detail"))
